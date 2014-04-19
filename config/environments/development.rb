8cca6721c6fb9695782bce67bb0da457fadf4252
Modsky1 <maude.jake@gmail.com>
2014-04-19 14:16:15 -0400

    adjusted Redis to go in dev environs and initializers

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -27,5 +27,8 @@ HomedIn::Application.configure do
   # number of complex assets.
   config.assets.debug = true
 
-         config.action_mailer.default_url_options = { host: 'localhost:3000' }
+  config.action_mailer.default_url_options = { host: 'localhost:3000' }
+
+  ENV["REDISTOGO_URL"] = 'redis://username:password@my.host:6789'
+
 end
