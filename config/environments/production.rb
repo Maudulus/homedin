dc1cf4efc0405a090333f2f51c6a29a38e8641d3
Modsky1 <maude.jake@gmail.com>
2014-04-21 20:36:11 -0400

    production.rb actionmailer

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -78,6 +78,6 @@ HomedIn::Application.configure do
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
 
-  config.action_mailer.default_url_options = { :host => 'homedin.herokuapp.com' }
+  config.action_mailer.default_url_options = { :host => 'homedin.heroku.com' }
 
 end
